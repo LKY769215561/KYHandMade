f2f4536a422f0c4e6aec77e1d4e86efe38097923
Kerain <kerain.liang@jiuzhang.net>
2017-06-08 17:30:46 +0800

    初始化

---
+++ KYHandMade/KYHandMade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KYHandMade
+//
+//  Created by Kerain on 2017/6/8.
+//  Copyright © 2017年 广州市九章信息科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
